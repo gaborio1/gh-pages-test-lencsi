d7c3dd01f589b0c5a1779d5d08f15640e7dfe40c
gaborio1 <welcome2thepleasuredome@googlemail.com>
2022-01-06 23:11:27 +0000

    clone to macbook add parcel -g

--- dist/transition.f6bb2fc7.js
+++ dist/transition.f6bb2fc7.js
@@ -6183,7 +6183,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57719" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50385" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
